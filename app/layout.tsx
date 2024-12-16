bc105eb477e930bbc778e2b045feac8b8e265488
Miga <migaa38@gmail.com>
2024-12-16 18:18:04 +0100

    add speedinsight

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google';
 import { Toaster } from 'sonner';
 import { Sidebar } from '@/components/sidebar';
 import { Providers } from './providers';
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -28,6 +28,7 @@ export default function RootLayout({
             </main>
           </div>
           <Toaster />
+          <SpeedInsights />
         </Providers>
       </body>
     </html>
